8e204d496ac8deaf13afdfaf198873ee0e3b1cd1
Mikolaenko Vadim <vmikolaenko@hse.ru>
2025-12-10 19:52:05 +0300

    Animation of plane rotation

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ from rotation_matrixes import *
 from scipy.spatial.transform import Rotation as R
 
 airplane_0 = A_z(90) @ airplane
-
-airplane_now = airplane_0
 draw(airplane_0)
-plt.pause(5)
-airplane_now = A_y(90) @ airplane_now
-draw(airplane_now)
-plt.pause(5)
-airplane_now = A_x(90) @ airplane_now
-draw(airplane_now)
-plt.pause(5)
-airplane_now = A_z(120) @ airplane_now
-draw(airplane_now)
-plt.pause(5)
+plt.waitforbuttonpress()
+
+airplane_i = airplane_0
+for i in range(36):
+    airplane_i = A_z(i*10) @ airplane_0
+    draw(airplane_i)
+    plt.waitforbuttonpress()
